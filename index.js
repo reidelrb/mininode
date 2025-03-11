bc615aa8f32291923d5d7c874f680afbbcedf09e
Reidel Rojas <re94e12@gmail.com>
2025-03-11 22:47:51 +0100

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 
 const express = require('express');
    const app = express();
-   const port = process.env.PORT || 3000;
+   const port = process.env.PORT || 1000;
 
    // Middleware para parsear JSON
    app.use(express.json());
 
    // Endpoint /new
    app.get('/new', (req, res) => {
-       res.json({ code: 'Ejemplo de código' });
+       res.json({ code: 're57hem' });
    });
 
    // Endpoint /list
-   app.get('/list', (req, res) => {
+   app.get('/all', (req, res) => {
        res.json({ list: [] });
    });
 
    // Endpoint /update
-   app.post('/update', (req, res) => {
+   app.post('/up', (req, res) => {
        res.json({ success: true });
    });
 
